844650f6c7fb12db9779225a56e421b2534e3666
Rick Kerkhof <rick.2889@gmail.com>
2012-12-31 02:32:49 +0100

    Sample project added

---
+++ Sample Project/Package/install.php
@@ -0,0 +1,6 @@
+<?php
+
+// Some custom code that can be inserted here. This code is a VERY BAD practice.
+echo "Thank you for installing this wonderful modification.";
+
+?>
---
+++ Sample Project/Package/uninstall.php
@@ -0,0 +1,6 @@
+<?php
+
+// Some custom code that can be inserted here. This code is a VERY BAD practice.
+echo "We are sorry to see you leave; did something go wrong?";
+
+?>
